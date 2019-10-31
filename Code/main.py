96c68d458d5297f1637ffdd03377401c9b689f43
Carpenter, John M <jmcarpenter@mitre.org>
2019-10-30 22:08:07 -0400

    Commit some more code

--- Code/main.py
+++ Code/main.py
@@ -4,13 +4,16 @@ import csv
 from matplotlib import pyplot as plt
 import readDat
 import filePath
-
+import os
 def main():
+    os.chdir(filePath.path["path"])
     # CSV file path here
     # You may need to change your path in the filePath.py file!
     path = filePath.path["path"]
     # Get the CSV files using readDat
     file_paths = readDat.glob_file(path)
     # read each csv into a pandas df
+    readDat.read_data(file_paths)
+
 if __name__=="__main__":
     main()
--- Code/readDat.py
+++ Code/readDat.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import glob
-
+import numpy as np
 # Use glob to get the realtive paths of the csv files
 def glob_file(path_to_csv):
     files = glob.glob1(path_to_csv , "*.csv")
     return files
 # Read data into pandas dataframe function
-def read_data(data_path):
-    df = pd.read_csv(r"{}".format(data_path))
-    return df
+def read_data(data_file_list):
+    for file in range(len(data_file_list)):
+        df = pd.read_csv(r"{}".format(file))
+        print(df)
 
