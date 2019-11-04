4065f409d00b8f098455fd32fd96749d1da9f006
Carpenter, John M <jmcarpenter@mitre.org>
2019-11-03 19:23:27 -0500

    Looked at the keys from each data frame. Need to figure out how we want to drop certain columns or delete certain rows

--- Code/main.py
+++ Code/main.py
@@ -14,14 +14,19 @@ def main():
     # CSV file path here
     # User will need to change the path of where the CSV files live.
     # read each csv into a pandas dataframes
-    try:
-        df2015 = readDat.csv_to_dataframe(r"C:\Users\jmcarpenter\Desktop\UVA Graduate School Stuff\CS_DataScience_5010\CS_5010\CS_Project\HappinessAnalysis\2015.csv")
-        df2016 = readDat.csv_to_dataframe(r"C:\Users\jmcarpenter\Desktop\UVA Graduate School Stuff\CS_DataScience_5010\CS_5010\CS_Project\HappinessAnalysis\2016.csv")
-        df2017 = readDat.csv_to_dataframe(r"C:\Users\jmcarpenter\Desktop\UVA Graduate School Stuff\CS_DataScience_5010\CS_5010\CS_Project\HappinessAnalysis\2017.csv")
-    except:
-        print("Failed to read a csv file to pandas dataframe!")
-    ###############################
-    ###############################
+    df2015 = readDat.csv_to_dataframe(r"C:\Users\jmcarpenter\Desktop\UVA Graduate School Stuff\CS_DataScience_5010\CS_5010\CS_Project\HappinessAnalysis\2015.csv")
+    df2016 = readDat.csv_to_dataframe(r"C:\Users\jmcarpenter\Desktop\UVA Graduate School Stuff\CS_DataScience_5010\CS_5010\CS_Project\HappinessAnalysis\2016.csv")
+    df2017 = readDat.csv_to_dataframe(r"C:\Users\jmcarpenter\Desktop\UVA Graduate School Stuff\CS_DataScience_5010\CS_5010\CS_Project\HappinessAnalysis\2017.csv")
+    # Need to look into each CSV and find the discrepancies
+    # Get the keys
+    print(df2015.keys()) # ['Country', 'Region', 'Happiness Rank', 'Happiness Score','Standard Error', 'Economy (GDP per Capita)', 'Family','Health (Life Expectancy)', 'Freedom', 'Trust (Government Corruption)','Generosity', 'Dystopia Residual']
+    print(df2016.keys()) # ['Country', 'Region', 'Happiness Rank', 'Happiness Score','Lower Confidence Interval', 'Upper Confidence Interval', 'Economy (GDP per Capita)', 'Family', 'Health (Life Expectancy)','Freedom', 'Trust (Government Corruption)', 'Generosity', 'Dystopia Residual']
+    print(df2017.keys()) # ['Country', 'Happiness.Rank', 'Happiness.Score', 'Whisker.high', 'Whisker.low', 'Economy..GDP.per.Capita.', 'Family', 'Health..Life.Expectancy.', 'Freedom', 'Generosity', 'Trust..Government.Corruption.', 'Dystopia.Residual']
+    # We can see that the keys vary from df to df
+    print(df2015["Country"])
+    print(df2015["Country"].isin(df2016["Country"]))
 
 if __name__=="__main__":
     main()
+
+
--- Code/readDat.py
+++ Code/readDat.py
@@ -7,7 +7,10 @@ def glob_file(path_to_csv):
     return files
 # Read data into pandas dataframe function
 def csv_to_dataframe(file_path):
-    df = pd.read_csv(file_path)
+    try:
+        df = pd.read_csv(file_path)
+    except NotADirectoryError as err:
+        print("That's not a directory on this machine.")
     return df
 
 
