e2fdee204f2d30ff40a40f1b9f1fd15b9ae0792c
Carpenter, John M <jmcarpenter@mitre.org>
2019-11-13 21:03:49 -0500

    append works

--- Code/main.py
+++ Code/main.py
@@ -19,12 +19,21 @@ def main():
     df2017 = readDat.csv_to_dataframe(r"C:\Users\jmcarpenter\Desktop\UVA Graduate School Stuff\CS_DataScience_5010\CS_5010\CS_Project\HappinessAnalysis\2017.csv")
     # Need to look into each CSV and find the discrepancies
     # Get the keys
-    print(df2015.keys()) # ['Country', 'Region', 'Happiness Rank', 'Happiness Score','Standard Error', 'Economy (GDP per Capita)', 'Family','Health (Life Expectancy)', 'Freedom', 'Trust (Government Corruption)','Generosity', 'Dystopia Residual']
-    print(df2016.keys()) # ['Country', 'Region', 'Happiness Rank', 'Happiness Score','Lower Confidence Interval', 'Upper Confidence Interval', 'Economy (GDP per Capita)', 'Family', 'Health (Life Expectancy)','Freedom', 'Trust (Government Corruption)', 'Generosity', 'Dystopia Residual']
-    print(df2017.keys()) # ['Country', 'Happiness.Rank', 'Happiness.Score', 'Whisker.high', 'Whisker.low', 'Economy..GDP.per.Capita.', 'Family', 'Health..Life.Expectancy.', 'Freedom', 'Generosity', 'Trust..Government.Corruption.', 'Dystopia.Residual']
+    #print(df2015.keys()) # ['Country', 'Region', 'Happiness Rank', 'Happiness Score','Standard Error', 'Economy (GDP per Capita)', 'Family','Health (Life Expectancy)', 'Freedom', 'Trust (Government Corruption)','Generosity', 'Dystopia Residual']
+    #print(df2016.keys()) # ['Country', 'Region', 'Happiness Rank', 'Happiness Score','Lower Confidence Interval', 'Upper Confidence Interval', 'Economy (GDP per Capita)', 'Family', 'Health (Life Expectancy)','Freedom', 'Trust (Government Corruption)', 'Generosity', 'Dystopia Residual']
+    #print(df2017.keys()) # ['Country', 'Happiness.Rank', 'Happiness.Score', 'Whisker.high', 'Whisker.low', 'Economy..GDP.per.Capita.', 'Family', 'Health..Life.Expectancy.', 'Freedom', 'Generosity', 'Trust..Government.Corruption.', 'Dystopia.Residual']
     # We can see that the keys vary from df to df
-    print(df2015["Country"])
-    print(df2015["Country"].isin(df2016["Country"]))
+    #print(df2015["Country"])
+    #print(df2015["Country"].isin(df2016["Country"]))
+    print(len(df2015.keys()))
+    print(len(df2016.keys()))
+    print(len(df2017.keys()))
+    df_new=df2015.append(df2016)
+    df_new=df_new.append(df2017)
+    print(df_new.keys())
+    print(len(df_new.keys()))
+
+
 
 if __name__=="__main__":
     main()
